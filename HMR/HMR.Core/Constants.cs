fdf1ffb720ce7467523dff147bd187400c393be1
Amr Badawy <amr.badawy@msn.com>
2018-08-02 02:06:01 +0300

    Move Connection string to AppSettings.config

---
+++ HMR/HMR.Core/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HMR.Core
+{
+    public static class Constants
+    {
+        public static string ConnectionString { get; set; }
+    }
+}
